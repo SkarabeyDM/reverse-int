65452e93ff970ed99ce61d37095a33a715a5c2f7
Jamal Muslimov <mechanoid0415@gmail.com>
2022-06-22 11:21:13 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function reverse (n) {
-  
+
+    if (n < 0){
+        n *= -1
+    }
+
+    const numToArr = (num) => {
+        let arr = []
+        while (num >= 1) {
+            arr.push(Math.floor(num % 10))
+            num /= 10
+        }
+        return arr.reverse()
+    }
+    
+    const arrToNumReverse = (arr) => {
+        let num = 0
+        for (let i = 0; i < arr.length; i++) {
+            const element = arr[i];
+            num += element * 10 ** i
+        }
+        return num
+    }
+    
+    return arrToNumReverse(numToArr(n))
 }
