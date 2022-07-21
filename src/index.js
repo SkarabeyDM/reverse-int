765345200ebeff300e209060cfb26841f2d7124d
Jamal Muslimov <mechanoid0415@gmail.com>
2022-07-21 21:08:32 +0300

    refactor: optimisation

--- src/index.js
+++ src/index.js
@@ -1,24 +1,13 @@
-module.exports = function reverse (n) {
-
+function reverse(n) {
+    let a = 0
     n *= n < 0 ? -1 : 1
-
-    const numToArr = (num) => {
-        let arr = []
-        while (num >= 1) {
-            arr.push(Math.floor(num % 10))
-            num /= 10
-        }
-        return arr.reverse()
+    for (let pos = 0; n >= 1; pos++) {
+        a = ~~(n % 10) + a * 10
+        n /= 10
     }
-    
-    const arrToNumReverse = (arr) => {
-        let num = 0
-        for (let i = 0; i < arr.length; i++) {
-            const element = arr[i];
-            num += element * 10 ** i
-        }
-        return num
-    }
-
-    return arrToNumReverse(numToArr(n))
+    return a
 }
+
+module.exports = reverse
+
+console.log(reverse(225))
